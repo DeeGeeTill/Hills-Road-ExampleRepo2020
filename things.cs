41b35a39d27333329c397961b1f885c1458597e9
degeo <34075000+DeeGeeTill@users.noreply.github.com>
2020-09-17 14:22:26 +0100

    Create things.cs

---
+++ things.cs
@@ -0,0 +1,14 @@
+using System; 
+  
+namespace HelloWorldApp { 
+      
+    class Hills { 
+          
+
+        static void Main(string[] args) { 
+              
+            Console.WriteLine("Hello Hills"); 
+            Console.ReadKey(); 
+        } 
+    } 
+} 
